3b25b8631d4e57c55ff2cab1e77da6226fc0bc1e
johanarios <iohakarin06@gmail.com>
2013-11-16 04:19:04 -0500

    Update SolicitudServiceImpl.java
    v4

--- src/main/java/com/proyecto/siswebastec/servicesImpl/SolicitudServiceImpl.java
+++ src/main/java/com/proyecto/siswebastec/servicesImpl/SolicitudServiceImpl.java
@@ -1,9 +1,11 @@
 package com.proyecto.siswebastec.servicesImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.proyecto.siswebastec.DAO.SolicitudDAO;
 import com.proyecto.siswebastec.DAO.JPAUtil;
+import com.proyecto.siswebastec.model.Estado;
 import com.proyecto.siswebastec.model.Solicitud;
 import com.proyecto.siswebastec.services.SolicitudService;
 
@@ -47,7 +49,51 @@ public class SolicitudServiceImpl implements SolicitudService{
 		solicitudDAO = new SolicitudDAO(objJpaUtil.getEntityManager());
 		return solicitudDAO.listarTodos();
 	}
+	
+	public List<Solicitud> getSolicitudesPendientes() {
+		List<Solicitud> todos=getSolicitudes();
+		List<Solicitud> pendientes=new ArrayList<>();
+		//Estado pend=new Estado(1);
+		for(int i=0;i<todos.size();i++){
+			
+			if(todos.get(i).getIdEstado()!= null){
+				System.out.println(todos.get(i).getIdEstado().getIdEstado());
+				if(todos.get(i).getIdEstado().getIdEstado()==1){				
+					pendientes.add(todos.get(i));
+				}
+			}
+		}
+		return pendientes;
+	}
 
+	public List<Solicitud> getSolicitudesProceso() {
+		List<Solicitud> todos=getSolicitudes();
+		List<Solicitud> proceso=new ArrayList<>();
+		//Estado pend=new Estado(1);
+		for(int i=0;i<todos.size();i++){
+			if(todos.get(i).getIdEstado()!= null){
+				if(todos.get(i).getIdEstado().getIdEstado()==2){
+					proceso.add(todos.get(i));
+				}
+			}
+		}
+		return proceso;
+	}
+	
+	public List<Solicitud> getSolicitudesFinalizadas() {
+		List<Solicitud> todos=getSolicitudes();
+		List<Solicitud> finalizadas=new ArrayList<>();
+		//Estado pend=new Estado(1);
+		for(int i=0;i<todos.size();i++){
+			if(todos.get(i).getIdEstado()!= null){
+				if(todos.get(i).getIdEstado().getIdEstado()==3){
+					finalizadas.add(todos.get(i));
+				}
+			}
+		}
+		return finalizadas;
+	}
+	
 	public boolean verificaSolicitud(int id) {
 		// TODO Auto-generated method stub
 		return false;
