a0c9c88ed0315f8cae619c84948625935a9ae53e
johanarios <iohakarin06@gmail.com>
2013-11-16 04:12:17 -0500

    Update TableBean.java
    v2

--- src/main/java/com/proyecto/siswebastec/managedbeans/TableBean.java
+++ src/main/java/com/proyecto/siswebastec/managedbeans/TableBean.java
@@ -9,28 +9,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TableBean implements Serializable{
-	List<Solicitud> solicitudes;
+	List<Solicitud> solicitudes,solicitudespend,solicitudespro,solicitudesfin;
 	SolicitudService solserv;
 	public TableBean(){
 		solicitudes=new ArrayList<Solicitud>();
 		solserv=new SolicitudServiceImpl();
-		solicitudes=solserv.getSolicitudes();
-		System.out.println("Holitas!");
+		//solicitudes=solserv.getSolicitudes();
+		
+//		System.out.println("Holitas!");
 	}
 	
+	public List<Solicitud> getSolicitudespend() {
+		setSolicitudespend(solserv.getSolicitudesPendientes());
+		return solicitudespend;
+	}
+
+	public void setSolicitudespend(List<Solicitud> solicitudespend) {
+		this.solicitudespend = solicitudespend;
+	}
+
+	public List<Solicitud> getSolicitudespro() {
+		return solicitudespro;
+	}
+
+	public void setSolicitudespro(List<Solicitud> solicitudespro) {
+		this.solicitudespro = solicitudespro;
+	}
+
+	public List<Solicitud> getSolicitudesfin() {
+		return solicitudesfin;
+	}
+
+	public void setSolicitudesfin(List<Solicitud> solicitudesfin) {
+		this.solicitudesfin = solicitudesfin;
+	}
+
 	public List<Solicitud> getSolicitudes() {
 		return solicitudes;
 	}
+	
 	public void setSolicitudes(List<Solicitud> solicitudes) {
 		this.solicitudes = solicitudes;
 	}
+	
 	public SolicitudService getSolserv() {
 		return solserv;
 	}
+	
 	public void setSolserv(SolicitudService solserv) {
 		this.solserv = solserv;
 	}
 	
+	public void AsignarTecnico(){
+		
+		
+//		solserv.getSolicitudById(id);
+	}
+	
+	public void SeleccionaSolicitud(){
+		
+	}
 	
 	
 }
